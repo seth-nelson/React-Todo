f956090f706acc7b6823d0e508bbcdf76f68997e
Seth Nelson <seth.nelson@outlook.com>
2020-03-15 16:56:00 -0700

    adding other files to be caught up with where I was before in the project

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,24 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+
+const TodoList = (props) => {
+    return (
+        <div className='list-container'>
+            {props.TodoList.map(todo => (
+                <Todo 
+                    key={todo.id}
+                    task={todo}
+                    toggleCompleted={props.toggleCompleted}
+                />)
+            )}
+        {/* <button className='completed-button' onClick={props.clearCompleted}>
+            Tasks Completed
+        </button> */}
+        </div>
+    );
+}
+
+export default TodoList;
